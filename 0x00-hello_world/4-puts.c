69c6ba1615cc1887676e16e77ee1f10b69e4ce49
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-06-20 09:41:10 -0700

    Add C program and 2-line bash script to compile and run it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c -o 4-puts && ./4-puts
+#include <stdio.h>
+int main(void){puts("\"Programming is like building a multilingual puzzle");return (0);}
