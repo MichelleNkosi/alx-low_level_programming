4a73f99b53e109f103af82b4434282d8c2a9b37b
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-06-21 06:50:37 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers using putchar
+ * main prints all possible combinations of single-digit numbers using putchar
  *
  * Return: Always 0 (Success)
  */
