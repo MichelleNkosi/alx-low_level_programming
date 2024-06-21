b276f5d3eb76601a172397e25c6dad6867a68b42
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-06-21 06:21:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit = 0;
+
+while (digit <= 9)
+{
+putchar(digit % 10 + '0');
+digit++;
+}
+
+putchar('\n'); /* Print newline at the end */
+
+return (0);
+}
