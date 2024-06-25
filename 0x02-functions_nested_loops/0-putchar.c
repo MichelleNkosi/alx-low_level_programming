7232ab31271ef55c3748352010acb5cb9a2738fb
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-06-25 16:47:52 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * main - Entry point of the program
  *
  * This function prints "_putchar" followed by a new line using multiple
@@ -10,15 +10,15 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+_putchar('_');   /* Print '_' */
+_putchar('p');   /* Print 'p' */
+_putchar('u');   /* Print 'u' */
+_putchar('t');   /* Print 't' */
+_putchar('c');   /* Print 'c' */
+_putchar('h');   /* Print 'h' */
+_putchar('a');   /* Print 'a' */
+_putchar('r');   /* Print 'r' */
+_putchar('\n');  /* Print newline */
 
-return (0);
+return (0);  /* Return 0 to indicate success */
 }
