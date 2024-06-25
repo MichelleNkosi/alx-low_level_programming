4f8e928b09c7f754d153debf5cf21ffa01e86b5c
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-06-25 18:55:33 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,8 +2,6 @@
 
 /**
  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- *        followed by a new line.
- *
  * Return: Always 0.
  */
 int main(void)
@@ -13,9 +11,7 @@ unsigned long fib1 = 1, fib2 = 2;
 unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 unsigned long max = 1000000000; /* 10^9 */
-
 printf("%lu, %lu", fib1, fib2);
-
 for (count = 3; count <= 98; count++)
 {
 if (fib1 + fib2 < max)
@@ -30,23 +26,18 @@ fib1_half1 = fib1 / max;
 fib1_half2 = fib1 % max;
 fib2_half1 = fib2 / max;
 fib2_half2 = fib2 % max;
-
 half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
-
 if (half2 >= max)
-            {
+{
 half1 += 1;
 half2 -= max;
 }
-
 printf(", %lu%09lu", half1, half2);
-
 fib1 = fib2;
-fib2 = half1 * max + half2;
+fib2 = half1 *max + half2;
 }
 }
-
 printf("\n");
 return (0);
 }
