335c0bdc0900f1a600edb0abf6733911ef82ada6
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-06-25 08:52:37 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,5 +15,5 @@ if (last_digit < 0)
 last_digit = -last_digit; /* Make it positive if n is negative */
 
 _putchar('0' + last_digit);  /* Print the last digit */
-return last_digit;
+return (last_digit);
 }
