1883adb9ae6ae68cc089abb4b01fc26e16b8df24
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-06-25 10:00:26 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,42 +9,17 @@
 int main(void)
 {
 int count;
-unsigned long fib1 = 1, fib2 = 2;
-unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-unsigned long half1, half2;
-unsigned long max = 1000000000; /* 10^9 */
+unsigned long fib1 = 1, fib2 = 2, next;
 
 printf("%lu, %lu", fib1, fib2);
 
 for (count = 3; count <= 98; count++)
 {
-if (fib1 + fib2 < max)
-{
-fib2 = fib1 + fib2;
-fib1 = fib2 - fib1;
-printf(", %lu", fib2);
-}
-else
-{
-fib1_half1 = fib1 / max;
-fib1_half2 = fib1 % max;
-fib2_half1 = fib2 / max;
-fib2_half2 = fib2 % max;
-
-half1 = fib1_half1 + fib2_half1;
-half2 = fib1_half2 + fib2_half2;
-
-if (half2 >= max)
-            {
-half1 += 1;
-half2 -= max;
-}
-
-printf(", %lu%09lu", half1, half2);
-
+next = fib1 + fib2;
+printf(", %lu", next);
+        
 fib1 = fib2;
-fib2 = half1 * max + half2;
-}
+fib2 = next;
 }
 
 printf("\n");
