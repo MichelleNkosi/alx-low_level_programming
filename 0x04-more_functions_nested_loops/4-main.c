451f6bbaca250503eca16beed470be32368149c9
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-06-27 12:29:24 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point, tests the print_most_numbers function.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
