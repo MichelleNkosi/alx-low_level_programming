9f9dbb8e61fd4aef619246ae2f33cc18e7663cd7
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-07-01 20:45:50 +0200

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,6 @@
 #include "main.h"
-
 /**
  * print_array - prints n elements of an array of integers,
- * followed by a new line.
  * @a: pointer to the array
  * @n: number of elements to print
  *
@@ -13,17 +11,14 @@ void print_array(int *a, int n)
 int i;
 char buffer[12];  /* Buffer to hold string representation of numbers */
 int j, k;
-
 for (i = 0; i < n; i++)
 {
-        /* Convert integer to string */
 k = 0;
 if (a[i] < 0)
 {
 buffer[k++] = '-';
 a[i] = -a[i];
 }
-
 if (a[i] == 0)
 {
 buffer[k++] = '0';
@@ -34,24 +29,18 @@ int num = a[i];
 int divisor = 1000000000; /* Large divisor to get the most significant digit */
 while (divisor > num)
 divisor /= 10;
-
 while (divisor >= 1)
 {
-buffer[k++] = (num / divisor) + '0';
+buffer[k++] = (num / divisor) +'0';
 num %= divisor;
 divisor /= 10;
 }
 }
-
 buffer[k] = '\0';
-
-        /* Print the number */
 for (j = 0; buffer[j] != '\0'; j++)
 {
 _putchar(buffer[j]);
 }
-
-        /* Print comma and space if not the last element */
 if (i != n - 1)
 {
 _putchar(',');
