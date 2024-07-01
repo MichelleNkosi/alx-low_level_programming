fad2e117bf7ae38c5107666d19b7d34e0e95e2b6
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-07-01 11:32:01 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,38 @@ void print_array(int *a, int n)
 {
 int i;
 char buffer[12];  /* Buffer to hold string representation of numbers */
-int j;
+int j, k;
 
 for (i = 0; i < n; i++)
 {
-_itoa(a[i], buffer);
+        /* Convert integer to string */
+k = 0;
+if (a[i] < 0)
+{
+buffer[k++] = '-';
+a[i] = -a[i];
+}
+
+if (a[i] == 0)
+{
+buffer[k++] = '0';
+}
+else
+{
+int num = a[i];
+int divisor = 1000000000; /* Large divisor to get the most significant digit */
+while (divisor > num)
+divisor /= 10;
+
+while (divisor >= 1)
+{
+buffer[k++] = (num / divisor) + '0';
+num %= divisor;
+divisor /= 10;
+}
+}
+
+buffer[k] = '\0';
 
         /* Print the number */
 for (j = 0; buffer[j] != '\0'; j++)
