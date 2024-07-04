2a6a41d64e76af5058806cbaa3aca438b33bcb9d
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-07-04 08:55:25 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: pointer to the string
+ *
+ * Return: the pointer to the resulting string
+ */
+char *leet(char *str)
+{
+int i = 0;
+    
+while (str[i])
+{
+if (str[i] == 'a' || str[i] == 'A')
+str[i] = '4';
+else if (str[i] == 'e' || str[i] == 'E')
+str[i] = '3';
+else if (str[i] == 'o' || str[i] == 'O')
+str[i] = '0';
+else if (str[i] == 't' || str[i] == 'T')
+str[i] = '7';
+else if (str[i] == 'l' || str[i] == 'L')
+str[i] = '1';
+i++;
+}
+
+return (str);
+}
