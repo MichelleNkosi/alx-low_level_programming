029dc5a17bb19e5fc97bc86bcd3d6775567e3f03
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-07-10 10:44:25 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int guess);
 int is_prime_number(int n);
 int _is_prime_helper(int n, int divisor);
+int is_palindrome(char *s);
+int _is_palindrome_helper(char *s, int start, int end);
 
 #endif /* MAIN_H */
