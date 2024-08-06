c8644af401cd3b1a5a66bbfc434beb25d41db6c3
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-08-06 22:34:55 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,6 @@
 /**
  * listint_len - Returns the number of elements in a listint_t list.
  * @h: Pointer to the head of the list.
- * 
  * Return: The number of elements in the list.
  */
 size_t listint_len(const listint_t *h)
@@ -14,6 +13,5 @@ while (h != NULL)
 count++;
 h = h->next;
 }
-
 return (count);
 }
