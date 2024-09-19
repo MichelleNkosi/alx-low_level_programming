678da7ae7d24e3970795b0d67840308f9b75297f
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-09-19 09:11:00 -0700

    1-dlistint_len, 2-add_dnodeint, 3-add_dnodeint_end, 4-free_dlistint, 5-get_dnodeint, 6-sum_dlistint, 7-insert_dnodeint, 8-delete_dnodeint

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -26,7 +26,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-void print_dlistint(const dlistint_t *h);
 
 
 #endif /* LISTS_H */
