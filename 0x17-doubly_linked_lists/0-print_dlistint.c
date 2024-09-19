320fa994df78946e3a193471b680e3b95c0efc62
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-09-19 09:02:00 -0700

    1-dlistint_len, 2-add_dnodeint, 3-add_dnodeint_end, 4-free_dlistint, 5-get_dnodeint, 6-sum_dlistint, 7-insert_dnodeint, 8-delete_dnodeint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,17 @@
  *
  * Return: the number of nodes in the list
  */
-void print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+size_t count = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+count++;
+h = h->next;
 }
+
+return (count);
+}
+
